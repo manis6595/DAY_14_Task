9aad98ba5a0e955d83a6fb767ab2d31d627ef43f
manis6595 <143244959+manis6595@users.noreply.github.com>
2023-10-17 14:33:22 +0530

    Add files via upload
    Updated

--- Form & Table _Task/table.js
+++ Form & Table _Task/table.js
@@ -1,7 +1,7 @@
 document.getElementById('dataform').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Get form values
+    
     const firstName = document.getElementById('validationTooltip01').value;
     const lastName = document.getElementById('validationTooltip02').value;
     const address = document.getElementById('address').value;
@@ -13,7 +13,7 @@ document.getElementById('dataform').addEventListener('submit', function(event) {
     const state = document.getElementById('validationTooltip04').value;
     const country = document.getElementById('country').value;
 
-    // Create table row and cells
+  
     const tableRow = document.createElement('tr');
     const tableData = [firstName, lastName, address, pincode, gender, foods.join(', '), state, country];
     tableData.forEach(data => {
@@ -22,24 +22,10 @@ document.getElementById('dataform').addEventListener('submit', function(event) {
         tableRow.appendChild(cell);
     });
 
-    // Append row to the table
+
     const dataTable = document.getElementById('dataTable');
     dataTable.appendChild(tableRow);
 
-    // Reset form fields
+  
     document.getElementById('dataform').reset();
 });
-//     function validateForm() {
-//     var x = document.forms["dataform"]["validationTooltip01"].value;
-//     var x = document.forms["dataform"]["validationTooltip02"].value;
-//     var x = document.forms["dataform"]["address"].value;
-//     var x = document.forms["dataform"]["validationTooltip05"].value;
-//     var x = document.forms["dataform"]["validationTooltip01"].value;
-//     var x = document.forms["dataform"]["country"].value;
-//     let x = document.forms["dataform"]["gender"].value;
-//     let x = document.forms["dataform"]["validationTooltip01"].value;
-//     if (x == "") {
-//       alert("Name must be filled out");
-//       return false;
-//     }
-//   }
